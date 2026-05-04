c53e43cc20b2d7c240eff303f364a8b58d70963a
Claris Gu <55867918+jgu13@users.noreply.github.com>
2026-05-04 17:36:16 -0400

    Update project and binary paths in Global_parameters.py

--- scripts/Global_parameters.py
+++ scripts/Global_parameters.py
@@ -1,7 +1,7 @@
 import os
-PROJ_HOME = os.path.expanduser("~/projects/MiRformer")
-RNACOFOLD_BIN = "/home/mcb/users/jgu13/.conda/envs/HyenaDNA_clone2/bin/RNAcofold"
-RNAHYBRID_BIN = "/home/mcb/users/jgu13/.conda/envs/rnahybrid/bin/RNAhybrid"
+PROJ_HOME = os.path.expanduser("~/project/home")
+RNACOFOLD_BIN = "/path/to/RNAcofold"
+RNAHYBRID_BIN = "/path/to/RNAhybrid"
 AXIS_FONT_SIZE = 15
 TICK_FONT_SIZE = 15
 TITLE_FONT_SIZE = 18
