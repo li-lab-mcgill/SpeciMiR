6d1a1973af9eec7ab2af5616aeb2d4706d729e6a
Claris Gu <55867918+jgu13@users.noreply.github.com>
2026-05-04 17:56:42 -0400

    Modify save paths in plot_model_performance_line_plot.sh
    Updated paths for saving train loss, test accuracy, and test loss images to placeholder paths.

--- scripts/plot_model_performance_line_plot.sh
+++ scripts/plot_model_performance_line_plot.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
-
-# miRAW_noL_noMissing=/home/mcb/users/jgu13/projects/mirLM/Performance/miRAW_noL_noMissing
-# python scripts/plot_model_performance_line_plot.py \
-#     --mRNA_max_len 40\
-#     --dataset_name miRAW_noL_noMissing\
-#     --model_dirs MLP HyenaDNA CNN\
-#     --model_names_in_plot TwoTower-HyenaDNA-MLP-CrossAttn Finetuned-HyenaDNA-MLP Baseline-CNN \
-#     --train_loss_save_path $miRAW_noL_noMissing/miraw_noL_noMissing_train_loss.png \
-#     --test_acc_save_path $miRAW_noL_noMissing/miraw_noL_noMissing_test_acc.png
-
-# syn_data=/home/mcb/users/jgu13/projects/mirLM/Performance/perfect_seed_match
-# python scripts/plot_model_performance_line_plot.py \
-#     --mRNA_max_len 40\
-#     --dataset_name perfect_seed_match\
-#     --model_dirs MLP HyenaDNA CNN\
-#     --model_names_in_plot TwoTower-HyenaDNA-MLP-CrossAttn Finetuned-HyenaDNA-MLP Baseline-CNN \
-#     --train_loss_save_path $syn_data/perfect_seed_match.png \
-#     --test_acc_save_path $syn_data/miraw_noL_noMissing_test_acc.png
-
 python scripts/plot_model_performance_line_plot.py \
     --mRNA_max_len 500\
     --dataset_name TargetScan\
     --model_dirs HyenaDNA_miRNA_500 \
     --model_names Finetuned-HyenaDNA-500nt \
-    --train_loss_save_path /home/mcb/users/jgu13/projects/mirLM/Performance/TargetScan/HyenaDNA_miRNA_500/10/train_loss.png \
-    --test_acc_save_path /home/mcb/users/jgu13/projects/mirLM/Performance/TargetScan/HyenaDNA_miRNA_500/10/evaluation_acc.png \
-    --test_loss_save_path /home/mcb/users/jgu13/projects/mirLM/Performance/TargetScan/HyenaDNA_miRNA_500/10/evaluation_loss.png \
+    --train_loss_save_path /path/to/train_loss.png \
+    --test_acc_save_path /path/to/evaluation_acc.png \
+    --test_loss_save_path /path/to/evaluation_loss.png \
